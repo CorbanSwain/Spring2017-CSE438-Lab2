46eeb8d5f5013814c41873060105ff10eef7041f
Corban Swain <nabsterrock@gmail.com>
2017-02-06 11:57:41 -0600

    Initial Commit

---
+++ VirtuialPetCS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtuialPetCS
+//
+//  Created by Corban Swain on 2/6/17.
+//  Copyright © 2017 CorbanSwain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
